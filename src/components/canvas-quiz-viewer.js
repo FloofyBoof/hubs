fd74518bc712d5f8d41e7e715748f03d307f8af7
FloofyBoof <floofyboof@outlook.com>
2023-09-15 16:12:41 +1000

    Add text and placeholder API text

--- src/components/canvas-quiz-viewer.js
+++ src/components/canvas-quiz-viewer.js
@@ -1,26 +1,34 @@
-
-
 AFRAME.registerComponent("canvas-quiz-viewer", {
-  init: function() {
-    console.log("Canvas Quiz Viewer: Hello World");
+    init: function() {
+        console.log("Canvas Quiz Viewer: Hello World");
 
-    async function fetchData() {
-      const response = await fetch("https://jsonplaceholder.typicode.com/todos/1", {
-        method: "GET"
-      });
-      const data = await response.json();
+        this.boxMesh = new THREE.Mesh(
+            new THREE.BoxGeometry(0.1, 0.1, 0.1),
+            new THREE.MeshPhysicalMaterial()
+        );
+        this.el.object3D.add(this.boxMesh);
 
-      this.textElement = document.createElement("a-entity");
-      this.textElement.setAttribute("text", `value: ${data.body}`);
-      this.el.appendChild(this.textElement);
+        this.textElement2 = document.createElement("a-entity");
+        this.textElement2.setAttribute("text", "value: This is the Canvas Quiz Cube custom component");
+        this.textElement2.setAttribute("position", "0 0.2 0");
+        this.el.appendChild(this.textElement2);
+
+        fetch("https://jsonplaceholder.typicode.com/posts/1",
+            { method: "GET" }
+        ).then(response => response.json())
+            .then(data => {
+                this.textElement = document.createElement("a-entity");
+                this.textElement.setAttribute("text", `value: ${data.title}`);
+                this.el.appendChild(this.textElement);
+            });
     }
+});
 
-    this.boxMesh = new THREE.Mesh(
-        new THREE.BoxGeometry(0.1, 0.1, 0.1),
-        new THREE.MeshPhysicalMaterial()
-    )
-    this.el.object3D.add(this.boxMesh);
+async function fetchData() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1", {
+        method: "GET"
+    });
+    const data = await response.json();
 
-    fetchData();
-  }
-})
+    return data;
+}
