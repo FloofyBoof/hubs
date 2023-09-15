af6713a115c4db39d74714f981fb0f40b0c3dbf8
FloofyBoof <floofyboof@outlook.com>
2023-09-15 19:19:05 +1000

    Use async API call

--- src/components/canvas-quiz-viewer.js
+++ src/components/canvas-quiz-viewer.js
@@ -1,34 +1,31 @@
 AFRAME.registerComponent("canvas-quiz-viewer", {
-    init: function() {
-        console.log("Canvas Quiz Viewer: Hello World");
+  init() {
+    console.log("Canvas Quiz Viewer: Hello World");
 
-        this.boxMesh = new THREE.Mesh(
-            new THREE.BoxGeometry(0.1, 0.1, 0.1),
-            new THREE.MeshPhysicalMaterial()
-        );
-        this.el.object3D.add(this.boxMesh);
+    this.boxMesh = new THREE.Mesh(
+      new THREE.BoxGeometry(0.1, 0.1, 0.1),
+      new THREE.MeshPhysicalMaterial()
+    );
+    this.el.object3D.add(this.boxMesh);
 
-        this.textElement2 = document.createElement("a-entity");
-        this.textElement2.setAttribute("text", "value: This is the Canvas Quiz Cube custom component");
-        this.textElement2.setAttribute("position", "0 0.2 0");
-        this.el.appendChild(this.textElement2);
+    this.textElement2 = document.createElement("a-entity");
+    this.textElement2.setAttribute("text", "value: This is the Canvas Quiz Cube custom component");
+    this.textElement2.setAttribute("position", "0 0.2 0");
+    this.el.appendChild(this.textElement2);
 
-        fetch("https://jsonplaceholder.typicode.com/posts/1",
-            { method: "GET" }
-        ).then(response => response.json())
-            .then(data => {
-                this.textElement = document.createElement("a-entity");
-                this.textElement.setAttribute("text", `value: ${data.title}`);
-                this.el.appendChild(this.textElement);
-            });
-    }
-});
+    this.fetchData();
+  },
 
-async function fetchData() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1", {
+  async fetchData() {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/1",
+      {
         method: "GET"
-    });
+      }
+    );
     const data = await response.json();
-
-    return data;
-}
+    this.textElement = document.createElement("a-entity");
+    this.textElement.setAttribute("text", `value: ${data.title}`);
+    this.el.appendChild(this.textElement);
+  }
+});
