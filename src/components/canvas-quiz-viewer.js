0eade1ec771fbc793da7f6f06d9d0f71811471ea
FloofyBoof <floofyboof@outlook.com>
2023-09-20 13:30:35 +1000

    Begin fetching quiz data with Canvas API

--- src/components/canvas-quiz-viewer.js
+++ src/components/canvas-quiz-viewer.js
@@ -1,31 +1,50 @@
+import axios from "axios";
+import { API_BASE_URL, API_KEY } from "../../api-config";
+
+axios.defaults.baseURL = API_BASE_URL;
+
+const COURSE_ID = 70814;
+
 AFRAME.registerComponent("canvas-quiz-viewer", {
   init() {
     console.log("Canvas Quiz Viewer: Hello World");
 
-    this.boxMesh = new THREE.Mesh(
-      new THREE.BoxGeometry(0.1, 0.1, 0.1),
-      new THREE.MeshPhysicalMaterial()
-    );
-    this.el.object3D.add(this.boxMesh);
-
     this.textElement2 = document.createElement("a-entity");
-    this.textElement2.setAttribute("text", "value: This is the Canvas Quiz Cube custom component");
+    this.textElement2.setAttribute("text", "value: This is the Canvas Quiz custom component");
     this.textElement2.setAttribute("position", "0 0.2 0");
     this.el.appendChild(this.textElement2);
 
-    this.fetchData();
+    this.getQuizData(COURSE_ID, 567161);
   },
 
-  async fetchData() {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/posts/1",
-      {
-        method: "GET"
-      }
-    );
-    const data = await response.json();
-    this.textElement = document.createElement("a-entity");
-    this.textElement.setAttribute("text", `value: ${data.title}`);
-    this.el.appendChild(this.textElement);
+  async getQuizData(courseId, quizId) {
+    const response = await axios.get(`courses/${courseId}/quizzes/${quizId}`);
+    const data = response.data;
+
+    if (response.status != 200) {
+      return;
+    }
+
+    const questions = await axios.get(`courses/${courseId}/quizzes/${quizId}/questions`);
+    const questionsData = questions.data;
+
+    const currentQuestion = questionsData[0];
+
+    // Generate quiz title
+    this.titleText = document.createElement("a-entity");
+    this.titleText.setAttribute("text", `value: ${data.title}`);
+    this.el.appendChild(this.titleText);
+
+    // Generate question text by stripping out HTML from API data
+    const html = currentQuestion.question_text;
+    const div = document.createElement("div");
+    div.innerHTML = html;
+    const questionText = div.textContent || div.innerText || "";
+
+    this.questionText = document.createElement("a-entity");
+    this.questionText.setAttribute("text", `value: ${questionText}`);
+    this.questionText.setAttribute("position", "0 -0.2 0");
+    this.el.appendChild(this.questionText);
+
   }
 });
