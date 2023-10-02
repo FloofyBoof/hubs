c5cdf12d126766e623cc21976393255ea3650e10
FloofyBoof <floofyboof@outlook.com>
2023-10-02 15:18:52 +1100

    Fetch answer text from quiz API

--- src/components/canvas-quiz-viewer.js
+++ src/components/canvas-quiz-viewer.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 import { API_BASE_URL } from "../../api-config";
 
 axios.defaults.baseURL = API_BASE_URL;
@@ -11,7 +12,8 @@ AFRAME.registerComponent("canvas-quiz-viewer", {
     console.log("Canvas Quiz Viewer: Hello World");
 
     this.textElement2 = document.createElement("a-entity");
-    this.textElement2.setAttribute("text", "value: This is the Canvas Quiz custom component");
+    this.textElement2.setAttribute(
+      "text", "value: This is the Canvas Quiz custom component");
     this.textElement2.setAttribute("position", "0 0.2 0");
     this.el.appendChild(this.textElement2);
 
@@ -26,7 +28,8 @@ AFRAME.registerComponent("canvas-quiz-viewer", {
       return;
     }
 
-    const questions = await axios.get(`courses/${courseId}/quizzes/${quizId}/questions`);
+    const questions =
+      await axios.get(`courses/${courseId}/quizzes/${quizId}/questions`);
     const questionsData = questions.data;
 
     const currentQuestion = questionsData[0];
@@ -37,14 +40,27 @@ AFRAME.registerComponent("canvas-quiz-viewer", {
     this.el.appendChild(this.titleText);
 
     // Generate question text by stripping out HTML from API data
-    const html = currentQuestion.question_text;
-    const div = document.createElement("div");
-    div.innerHTML = html;
-    const questionText = div.textContent || div.innerText || "";
+    const questionText = stripHtmlFromText(currentQuestion.question_text);
 
     this.questionText = document.createElement("a-entity");
     this.questionText.setAttribute("text", `value: ${questionText}`);
     this.questionText.setAttribute("position", "0 -0.2 0");
     this.el.appendChild(this.questionText);
+
+    // Generate answer text
+    currentQuestion.answers.forEach((answer, i) => {
+      displayAnswer(answer, i);
+    });
   }
 });
+
+function stripHtmlFromText(text) {
+  const div = document.createElement("div");
+  div.innerHTML = text;
+  return div.textContent || div.innerText || "";
+}
+
+function displayAnswer(answer, i) {
+  const text = answer.text;
+  console.log(text);
+}
